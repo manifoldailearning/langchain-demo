9db5b3f0ebadbabcedaa4ad6baa61df3695b3ee2
nachiketh <support@manifoldailearning.in>
2025-07-12 08:05:24 +0530

    Adds simple memory LangGraph example
    Creates a basic LangGraph example demonstrating a conversational flow.
    
    This example showcases a simple interaction between a human and an AI
    assistant with memory of the conversation.

---
+++ 4-langgraph/1-simple-memory.py
@@ -0,0 +1,22 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that can answer questions."),
+    ("placeholder", "{input}")
+])
+
+model = ChatOpenAI(model="gpt-4.1-nano", temperature=0.5)
+
+chain = prompt | model
+
+response = chain.invoke({
+    "input": [("human", "Ramesh won the cricket match yesterday."),
+                 ("ai","That's great news! Congratulations to Ramesh on winning the cricket match. It must have been an exciting game!"),
+                 ("human", "Who won cricket match yesterday where Ramesh was playing?")]
+})
+
+print(response.content)
